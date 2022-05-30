1daea5badf1d4393c91c12a8351908fc1f9ff7d2
Victor Machado <victor.machado.machado@gmail.com>
2022-05-30 18:17:45 +0200

    testing heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use('/', (req, res) =>{
     res.status(200).send({ status: "sucess", message: "Hello World", secret:`${process.env.SECRET}`})
 })
 
-const PORT = process.env.API_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
